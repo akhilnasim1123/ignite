0be204dffebd62b2be2eb60ae74b0c191d1de32f
akhilnasim <akhilnasim029@gmail.com>
2022-09-20 16:30:33 +0530

    ignite just completed

---
+++ main.js
@@ -0,0 +1,42 @@
+// console.log(document.head);
+// console.log(document.all);
+// console.log(document.all[11]);
+// console.log(document.forms);
+// console.log(document.doctype); 
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.footer);
+
+// var headtitle = document.getElementById('hey-text');
+//   headtitle.textContent='hello'
+// headtitle.innerHTML = "hey"
+// console.log(document.body);
+// headtitle.innerHTML = '<h5>hell0</h5>'
+
+// var classes= document.getElementsByClassName('row');
+// console.log(classes) 
+// classes[1].style.width ='100%'
+// classes[1].style.backgroundColor='red'
+// classes[1].style.height='60px'
+
+// classes[0].style.width ='100%'
+// classes[0].style.backgroundColor='yellow'
+// classes[0].style.height='60px'
+
+// classes[2].style.width ='100%'
+// classes[2].style.backgroundColor='black'
+// classes[2].style.height='60px'
+// classes[2].style.marginTop='60'
+
+// var input = document.querySelector('input')
+// input.value="go to the hell man"
+
+// var submit= document.querySelector('input[type=submit]')
+// submit.value='mmmmmmm'
+
+
+var form=document.querySelector('#reed'); 
+console.log(form.previousElementSibling);
+form.previousElementSibling.style.background='black';
+form.previousElementSibling.style.width='100%';
+form.previousElementSibling.style.height='100px';
